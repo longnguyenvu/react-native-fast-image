b39ac88edb1b5eb1879bf31a93466f116149cea2
Lucas Nguyen <longnguyenvu@outlook.com>
2021-07-26 13:59:32 +0700

    update cache memory

--- android/src/main/java/com/dylanvann/fastimage/FastImageGlideModule.java
+++ android/src/main/java/com/dylanvann/fastimage/FastImageGlideModule.java
@@ -2,8 +2,14 @@ package com.dylanvann.fastimage;
 
 import com.bumptech.glide.annotation.GlideModule;
 import com.bumptech.glide.module.AppGlideModule;
+package com.bumptech.glide.load.engine.cache.InternalCacheDiskCacheFactory;
 
 // We need an AppGlideModule to be present for progress events to work.
 @GlideModule
 public final class FastImageGlideModule extends AppGlideModule {
+  @Override
+  public void applyOptions(Context context, GlideBuilder builder) {
+    int diskCacheSizeBytes = 1024 * 1024 * 100; // 100 MB
+    builder.setDiskCache(new InternalCacheDiskCacheFactory(context, diskCacheSizeBytes));
+  }
 }
