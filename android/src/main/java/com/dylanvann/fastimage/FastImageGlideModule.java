7145d46a93dba5a623a511f7c8a6f4669a84a888
Lucas Nguyen <longnguyenvu@outlook.com>
2021-07-27 16:17:12 +0700

    update override function

--- android/src/main/java/com/dylanvann/fastimage/FastImageGlideModule.java
+++ android/src/main/java/com/dylanvann/fastimage/FastImageGlideModule.java
@@ -1,5 +1,10 @@
 package com.dylanvann.fastimage;
 
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+
+import com.bumptech.glide.GlideBuilder;
 import com.bumptech.glide.annotation.GlideModule;
 import com.bumptech.glide.module.AppGlideModule;
 import com.bumptech.glide.load.engine.cache.InternalCacheDiskCacheFactory;
@@ -8,8 +13,10 @@ import com.bumptech.glide.load.engine.cache.InternalCacheDiskCacheFactory;
 @GlideModule
 public final class FastImageGlideModule extends AppGlideModule {
   @Override
-  public void applyOptions(Context context, GlideBuilder builder) {
+  public void applyOptions(@NonNull @org.jetbrains.annotations.NotNull Context context, @NonNull @org.jetbrains.annotations.NotNull GlideBuilder builder) {
     int diskCacheSizeBytes = 1024 * 1024 * 100; // 100 MB
     builder.setDiskCache(new InternalCacheDiskCacheFactory(context, diskCacheSizeBytes));
   }
 }
+
+
